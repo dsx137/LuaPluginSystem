0072b5114da97f3310cfad0d035591fc4eeb809a
dsx137 <70027572+dsx137@users.noreply.github.com>
2023-10-19 05:44:48 +0800

    ref code

--- src/util.cpp
+++ src/util.cpp
@@ -41,3 +41,21 @@ std::string GetCurrentExecutableDirectory() {
 
     return directory;
 }
+
+// --Lua插件脚本示例
+// function CreatePluginInstance()
+// local plugin = {
+//     --实现插件接口方法
+//     Initialize = function()
+//         print("Plugin initialized")
+//     end,
+//     Update = function()
+//         print("Plugin updated")
+//     end,
+//     Uninitialize = function()
+//         print("Plugin uninitialized")
+//     end
+// }
+
+// return plugin
+// end
